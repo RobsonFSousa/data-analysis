e6cb797b29a5379ceb06320a9996dc0d7d191805
robson <nosbor.ferreira@gmail.com>
2020-10-14 20:31:53 -0300

    Reading files from a folder

--- src/main/java/br/com/dbc/data/analysis/Application.java
+++ src/main/java/br/com/dbc/data/analysis/Application.java
@@ -1,23 +1,21 @@
 package br.com.dbc.data.analysis;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.dbc.data.analysis.utils.FileUtil;
 
 @SpringBootApplication
 @RestController
 public class Application {
 	@Autowired
-	ResourceLoader resourceLoader;
+	FileUtil fileUtil;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -26,13 +24,7 @@ public class Application {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		
-		try {
-			String lines = new String(Files.readAllBytes(Paths.get(System.getProperty("user.home") + File.separator + "data" + File.separator + "in" + File.separator + "sale.dat")), StandardCharsets.ISO_8859_1);
-
-			System.out.println(lines);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		fileUtil.loadAllFiles(System.getProperty("user.home") + File.separator + "data" + File.separator + "in" + File.separator);
 		
 		return String.format("Hello %s!", name);
 	}
---
+++ src/main/java/br/com/dbc/data/analysis/utils/FileUtil.java
@@ -0,0 +1,41 @@
+package br.com.dbc.data.analysis.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileUtil {
+	
+	public Map<String, List<String>> loadAllFiles(String directory)
+	{
+		Map<String, List<String>> filesMap = new HashMap<String, List<String>>();
+		
+		try {
+			File path = new File(directory);
+			File files[] = path.listFiles();
+
+			for(File file : files) {
+				String filePath = file.getAbsolutePath();
+				String lines = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.ISO_8859_1);
+				List<String> list = new ArrayList<String>(Arrays.asList(lines.split("\n")));
+				
+				filesMap.put(filePath, list);
+	      }
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return filesMap;
+	}
+}
